2e7413e9c0601681dba18d365a95facac1f0658d
s-gokul-10 <gokul9865421248@gmail.com>
2025-07-30 21:52:16 +0530

    Create odd or even .java

---
+++ odd or even .java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("EVEN");
+        }
+            else{
+                System.out.println("ODD");
+            }
+                
+    }
+}
