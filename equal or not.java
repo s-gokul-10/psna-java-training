275c081823610d00b1edb5ebc4d384fd28810b8c
s-gokul-10 <gokul9865421248@gmail.com>
2025-07-30 21:50:13 +0530

    Create  equal or not.java

---
+++ equal or not.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        if(a==b&& b==a){
+            System.out.println("EQUAL");
+        }
+            else{
+                System.out.println("NOT EQUAL");
+            }
+        }
+}
