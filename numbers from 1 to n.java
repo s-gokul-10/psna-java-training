805f1e4afd85bf28a739b0ed464340f57387f90a
s-gokul-10 <gokul9865421248@gmail.com>
2025-07-30 22:08:59 +0530

    Create numbers from 1 to n.java

---
+++ numbers from 1 to n.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.print(i+" ");
+            
+        }
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
