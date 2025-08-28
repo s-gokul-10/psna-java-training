e100a3d069ba6413476aa0a01c2edaed13b93368
s-gokul-10 <gokul9865421248@gmail.com>
2025-08-28 22:00:07 +0530

    Create display their sum.java

---
+++ display their sum.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        
+        System.out.println("Sum of "+ a +" and " + b+" is "+(a+b));
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
