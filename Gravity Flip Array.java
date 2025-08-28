f8db4be33ff299dab814a09f420607ebde1da5ad
s-gokul-10 <gokul9865421248@gmail.com>
2025-08-28 21:38:08 +0530

    Create Gravity Flip Array.java

---
+++ Gravity Flip Array.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            
+        }
+        Arrays.sort(a);
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+       
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
