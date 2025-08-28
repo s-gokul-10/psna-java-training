1a4705cb8c5fee8c95fb84d0880aab27e438ef45
s-gokul-10 <gokul9865421248@gmail.com>
2025-08-28 21:50:24 +0530

    Create  length of the string.java

---
+++ length of the string.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.println(a.length());
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
