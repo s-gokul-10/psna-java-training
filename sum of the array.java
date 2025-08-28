932ae0d99131b379b34f0268d77179b5e1710d58
s-gokul-10 <gokul9865421248@gmail.com>
2025-08-28 20:59:18 +0530

    Create sum of the array.java

---
+++ sum of the array.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int sum=0;
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            sum=sum+a[i];
+        }
+        System.out.println(sum+" ");
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
