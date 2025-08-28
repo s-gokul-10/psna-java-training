915f9d7e3a5a05d55053577e250c4f80bedfb29d
s-gokul-10 <gokul9865421248@gmail.com>
2025-08-28 21:16:08 +0530

    Create Star Pattern.java

---
+++ Star Pattern.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n;j++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
