04e67a9a30105883c45be169735b4173beaff551
s-gokul-10 <gokul9865421248@gmail.com>
2025-08-28 21:43:07 +0530

    Create display the string in the password.java

---
+++ display the string in the password.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        for(int i=0;i<a.length();i++){
+            System.out.print("*");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
